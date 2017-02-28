f057bae6761238b57b358b45bdd0fec8a0fd64e2
abc <123456@qq.com>
2017-02-28 21:30:54 +0800

    查看讲师信息

--- js/teacher/list.js
+++ js/teacher/list.js
@@ -2,15 +2,32 @@
  * Created by MF on 2017/2/25.
  */
 define(['jquery','common','nprogress','template'],function ($, undefined, NProgress, template) {
+	//讲师列表
 	$.get('/v6/teacher',function (data) {
 		if(data.code == 200){
-			var html = template('teacher-list-tpl', {
-				list: data.result
-			});
+			var html = template('teacher-list-tpl', data);
 			$('#teacher-list-tbody').html(html);
 		}
 
+	});
+	/*
+	*查看讲师信息
+	* */
+	$('#teacher-list-tbody').on('click', '.teacher-view-btn',function () {
+		$.get('/v6/teacher/view', {
+			tc_id: $(this).parent().attr('data-id')
+		}, function (data) {
+			if(data.code == 200){
+				console.log(data.result);
+				var html = template('teacher-view-tpl', data.result);
+				$('#teacherModal').html(html);
+			}
+
+		});
 	})
+
+
+
 	/*进度加载完毕*/
 	NProgress.done();
 })
